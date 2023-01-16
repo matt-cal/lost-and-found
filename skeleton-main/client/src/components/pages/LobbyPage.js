14a3c2dccf886c886ba4e2af9166939442d63dca
Camila Zavala <alessandra050505@gmail.com>
2023-01-16 15:59:18 -0500

    lobby page progress

--- skeleton-main/client/src/components/pages/LobbyPage.js
+++ skeleton-main/client/src/components/pages/LobbyPage.js
@@ -1,16 +1,22 @@
+
+
 import React from "react";
 
 import "../../utilities.css";
 import "./LobbyPage.css";
 
 const LobbyPage = () => {
-  return (
-    <nav>
-      <div>
-        <span>Lost & Found</span>
-      </div>
-    </nav>
-  );
+    return (
+        <nav>
+            <div>
+                <span>
+                    Lost & Found
+                </span>
+            
+            </div>
+        </nav>
+    );
+
 };
 
 export default LobbyPage;
