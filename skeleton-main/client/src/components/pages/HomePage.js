f3ab9c463d7cae1bf46b08a36e3b6d5af4e57f6c
Matthew Callister <116524474+matt-cal@users.noreply.github.com>
2023-01-16 16:20:13 -0500

    Add initial styling to Home Page

--- skeleton-main/client/src/components/pages/HomePage.js
+++ skeleton-main/client/src/components/pages/HomePage.js
@@ -10,41 +10,36 @@ const GOOGLE_CLIENT_ID = "89445914111-u81dif2k5ba9g2bm0h5rvuotfo7f9tup.apps.goog
 
 const HomePage = ({ userId, handleLogin, handleLogout }) => {
   return (
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      {userId ? (
-        <div>
-          <button
-            onClick={() => {
-              googleLogout();
-              handleLogout();
-            }}
-          >
-            Logout
-          </button>
-          <Link to="/Lobby/">Play</Link>
-        </div>
-      ) : (
-        <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
-      )}
-      <h1>Lost & Found</h1>
-      <h2> What you need to change in this skeleton</h2>
-      <ul>
-        <li>
-          Change the Frontend CLIENT_ID (Skeleton.js) to your team's CLIENT_ID (obtain this at
-          http://weblab.us/clientid)
-        </li>
-        <li>Change the Server CLIENT_ID to the same CLIENT_ID (auth.js)</li>
-        <li>
-          Change the Database SRV (mongoConnectionURL) for Atlas (server.js). You got this in the
-          MongoDB setup.
-        </li>
-        <li>Change the Database Name for MongoDB to whatever you put in the SRV (server.js)</li>
-      </ul>
-      <h2>How to go from this skeleton to our actual app </h2>
-      <a href="https://docs.google.com/document/d/110JdHAn3Wnp3_AyQLkqH2W8h5oby7OVsYIeHYSiUzRs/edit?usp=sharing">
-        Check out this getting started guide
-      </a>
-    </GoogleOAuthProvider>
+    <div className="HomePage-container">
+      <div className="HomePage-title">Lost & Found</div>
+      <div className="HomePage-content">
+        <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+          {userId ? (
+            <div>
+              <button
+                onClick={() => {
+                  googleLogout();
+                  handleLogout();
+                }}
+              >
+                Logout
+              </button>
+              <Link to="/Lobby/">Play</Link>
+            </div>
+          ) : (
+            <div className="HomePage-signin-container u-flexColumn u-flex-justifyCenter u-flex-alignCenter ">
+              <p className="HomePage-signin-text">Log In</p>
+              <GoogleLogin
+                onSuccess={handleLogin}
+                onError={(err) => console.log(err)}
+                theme="filled_blue"
+                shape="pill"
+              />
+            </div>
+          )}
+        </GoogleOAuthProvider>
+      </div>
+    </div>
   );
 };
 
