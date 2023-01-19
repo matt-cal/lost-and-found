5fb609b13cdcc771361b0fdb2a23888aead9fe3f
Camila Zavala <alessandra050505@gmail.com>
2023-01-18 19:38:52 -0500

    Added things to the waiting room page

--- skeleton-main/client/src/components/pages/WaitingRoom.js
+++ skeleton-main/client/src/components/pages/WaitingRoom.js
@@ -5,9 +5,36 @@ import "./WaitingRoom.css";
 const WaitingRoom = ()=> {
     return(
     <div className="WaitingRoom-container">
-        <u1 className="side-Bar">
-            player 1
-        </u1>
+        <span className="left-Bar">
+            <div className="player-text">
+                Player 1
+            </div>
+            <div className="body-container">
+                Name:
+            </div>
+            <div className="body-container">
+                Ready
+            </div>
+            <div className="body-container">
+                Satistics
+            </div>
+        </span>
+        <span className="right-Bar">
+        <div className="player-text">
+                Player 2
+            </div>
+            <div className="body-container">
+                Name:
+            </div>
+            <div className="body-container">
+                <button>
+                    Ready
+                </button>
+            </div>
+            <div className="body-container">
+                Satistics
+            </div>
+        </span>
     </div>
     );
 };
