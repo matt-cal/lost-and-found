4cf42f68f796c2498553cdd2793f52e9164bdfa2
Matthew Callister <116524474+matt-cal@users.noreply.github.com>
2023-01-29 16:59:42 -0500

    Style enter key modal

--- client/src/components/pages/LobbyPage.js
+++ client/src/components/pages/LobbyPage.js
@@ -37,14 +37,36 @@ const LobbyPage = (props) => {
     });
   };
 
-  const EnterKeyModal = (
-    <span>
+  const UsernameModal = (
+    <div className="LobbyPage-username-container">
+      <div className="LobbyPage-username-display">
+        Username:
+        <div className="u-bold">{username}</div>
+      </div>
       <input
         type="text"
-        placeholder="Enter Key: XYWUDH"
+        placeholder="New Username"
+        className="LobbyPage-username-input"
+        name="message"
+        onChange={handleChange}
+        value={message}
+      />
+      <button className="LobbyPage-username-button" onClick={changeUsername}>
+        Change Username
+      </button>
+    </div>
+  );
+
+  const EnterKeyModal = (
+    <span className="LobbyPage-username-container">
+      <div className="LobbyPage-key-text u-bold">Enter Lobby Key:</div>
+      <input
+        type="text"
+        placeholder="ABCDEF"
         onChange={(e) => {
           setInputedKey(e.target.value);
         }}
+        className="LobbyPage-key-input"
       ></input>
       <button
         onClick={() => {
@@ -57,8 +79,17 @@ const LobbyPage = (props) => {
             }
           });
         }}
+        className="LobbyPage-key-submit"
       >
         Submit
+      </button>
+      <button
+        onClick={() => {
+          setShowModal(!showModal);
+        }}
+        className="LobbyPage-key-back"
+      >
+        Back
       </button>
     </span>
   );
@@ -89,24 +120,7 @@ const LobbyPage = (props) => {
             >
               Join
             </button>
-            {showModal ? EnterKeyModal : <span></span>}
-            <div className="LobbyPage-username-container">
-              <div className="LobbyPage-username-display">
-                Username:
-                <div>{username}</div>
-              </div>
-              <input
-                type="text"
-                placeholder="New Username"
-                className="LobbyPage-username-input"
-                name="message"
-                onChange={handleChange}
-                value={message}
-              />
-              <button className="LobbyPage-username-button" onClick={changeUsername}>
-                Change Username
-              </button>
-            </div>
+            {showModal ? EnterKeyModal : UsernameModal}
           </div>
         </div>
       </nav>
